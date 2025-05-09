ba8b0c97e03fd3911988313e49a83ec437596085
sahilkabir07 <sahilakthar8986@gmail.com>
2025-05-09 07:44:12 +0530

    Resume folder and hero section changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -45,7 +45,16 @@ const Hero = () => {
                             variants={childVariants} className='my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter'>{HERO_CONTENT}
                         </motion.p>
                         <motion.a
-                            variants={childVariants} href="/SahilAkhtarResume.pdf" target='blank' rel='noopener noreferrer' download className='bg-white rounded-full p-4 text-sm text-stone-800 mb-10'>Download Resume</motion.a>
+                            variants={childVariants}
+                            href="/SahilAkhtarResume.pdf"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            download
+                            className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10"
+                        >
+                            Download Resume
+                        </motion.a>
+
                     </motion.div>
 
                 </div>
